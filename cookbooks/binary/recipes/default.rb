9506e9c2e327e6b597db8c8d7609f9e62b40e0f7
Michal Papis <mpapis@gmail.com>
2012-10-17 08:50:34 +0200

    improve logging of recipe commands execution

--- cookbooks/binary/recipes/default.rb
+++ cookbooks/binary/recipes/default.rb
@@ -7,34 +7,43 @@
 # All rights reserved - Do Not Redistribute
 #
 
-class Chef::Resource::Bash
-  def extra_log(name)
-    "2>&1 | tee /var/log/#{name}.log" if node[:rvm][:debug]
+class Chef::Resource::Script
+  def log_code command
+    if node[:rvm][:debug]
+      code "{ #{command}; } 2>&1 | tee /var/log/#{@command.to_s.gsub(/ /,"_")}.log"
+    else
+      code command
+    end
   end
 end
 
 bash "install #{node["platform"]} requirements" do
-  code [
+  reqs = [
     node[:rvm][:requirements][ node["platform"]        ] ||
     node[:rvm][:requirements][ node["platform_family"] ] ||
     node[:rvm][:requirements][ node["os"]              ]
   ].flatten * " && "
+  if node[:rvm][:debug]
+    puts "--- type: #{ node["platform"] } / #{ node["platform_family"] } / #{ node["os"] }"
+    puts "--- requirements: #{ reqs }"
+  end
+  log_code reqs
 end
 
 bash "install rvm" do
-  code "curl -L https://get.rvm.io | bash -s -- --auto #{extra_log("rvm-install")}"
+  log_code "curl -L https://get.rvm.io | bash -s -- --auto"
 end
 
 node[:rvm][:binary][:versions].each do |version|
   bash "uninstall #{version}" do
-    code "/usr/local/rvm/bin/rvm uninstall #{version}"
+    log_code "/usr/local/rvm/bin/rvm uninstall #{version}"
     only_if "/usr/local/rvm/bin/rvm use #{version}"
   end
   bash "install #{version}" do
-    code "/usr/local/rvm/bin/rvm install #{version} --movable #{extra_log("rvm-install")}"
+    log_code "/usr/local/rvm/bin/rvm install #{version} --movable"
   end
   bash "package #{version}" do
     cwd "/vagrant"
-    code "/usr/local/rvm/bin/rvm prepare #{version} --path #{extra_log("rvm-install")}"
+    log_code "/usr/local/rvm/bin/rvm prepare #{version} --path"
   end
 end
