ab652e8dcafa37ceac1fe014a364e62496aa57be
Michal Papis <mpapis@gmail.com>
2012-12-22 23:07:06 +0100

    add dbm for travis / ubunut / 1.9.3, fix https://github.com/wayneeseguin/rvm/issues/1404

--- cookbooks/binary/attributes/default.rb
+++ cookbooks/binary/attributes/default.rb
@@ -2,7 +2,7 @@ default[:rvm][:binary][:versions] = %W( ruby-1.9.3-p194 ruby-1.9.3-p286 ruby-1.9
 default[:rvm][:requirements][:ubuntu] =
 default[:rvm][:requirements][:debian] = [
   "sudo apt-get --quiet --yes update",
-  "sudo apt-get --no-install-recommends --quiet --yes install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config"
+  "sudo apt-get --no-install-recommends --quiet --yes install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config libgdbm-dev"
 ]
 default[:rvm][:requirements][:gentoo] = [
   "sudo emerge libiconv readline zlib openssl curl git libyaml sqlite libxslt libtool gcc autoconf automake bison m4"
